e154aa276f8834b1406a4d022d1878872eb22fc3
Michał Składanowski <michal.skladanowski@melements.pl>
2020-11-26 11:38:43 +0100

    Change positioning on the payment methods list

--- paynow.php
+++ paynow.php
@@ -64,9 +64,10 @@ class Paynow extends PaymentModule
         if (!parent::install() ||
             !$this->createDbTables() ||
             !$this->createModuleSettings() ||
-            !$this->registerHooks()) {
+            !$this->registerHooks()) ||{
             return false;
         }
+        $this->updatePosition(Hook::getIdByName('displayPayment'), false, 1);
         return true;
     }
 
@@ -102,8 +103,11 @@ class Paynow extends PaymentModule
 
         if (version_compare(_PS_VERSION_, '1.7', 'lt')) {
             $registerStatus &= $this->registerHook('payment') && $this->registerHook('displayPaymentEU');
+            $this->updatePosition(Hook::getIdByName('displayPayment'), false, 1);
+            $this->updatePosition(Hook::getIdByName('displayPaymentEU'), false, 1);
         } else {
             $registerStatus &= $this->registerHook('paymentOptions');
+            $this->updatePosition(Hook::getIdByName('paymentOptions'), false, 1);
         }
 
         return $registerStatus;
