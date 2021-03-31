d837fdb7b4e9ed1d31242d3614fcff594d1f0e2b
Michał Składanowski <michal.skladanowski@melements.pl>
2021-03-31 15:35:13 +0200

    Disable disabled payment methods

--- paynow.php
+++ paynow.php
@@ -391,7 +391,7 @@ class Paynow extends PaymentModule
                                 'name' => $this->getPaymentMethodTitle($payment_method->getType()),
                                 'image' => $payment_method->getImage(),
                                 'id' => $payment_method->getId(),
-                                'status' => "DISABLED"
+                                'status' => $payment_method->getStatus()
                             ]);
                         }
                         $list[$payment_method->getType()] = $payment_method->getId();
