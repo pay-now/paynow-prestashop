7671c51c2a740470cd235963735dc736781b9af7
Michał Składanowski <michal.skladanowski@melements.pl>
2021-04-01 09:34:22 +0200

    Release 1.3.3

--- paynow.php
+++ paynow.php
@@ -32,7 +32,7 @@ class Paynow extends PaymentModule
     {
         $this->name = 'paynow';
         $this->tab = 'payments_gateways';
-        $this->version = '1.3.2';
+        $this->version = '1.3.3';
         $this->ps_versions_compliancy = ['min' => '1.6.0', 'max' => _PS_VERSION_];
         $this->author = 'mElements S.A.';
         $this->is_eu_compatible = 1;
