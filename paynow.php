ff1b32c89a31ca0755a91f18ca8a492a0eca2750
michalskladanowski <59174285+michalskladanowski@users.noreply.github.com>
2021-04-01 09:12:13 +0200

    Disable not available payment methods (#61)

--- paynow.php
+++ paynow.php
@@ -390,7 +390,8 @@ class Paynow extends PaymentModule
                             array_push($payment_options, [
                                 'name' => $this->getPaymentMethodTitle($payment_method->getType()),
                                 'image' => $payment_method->getImage(),
-                                'id' => $payment_method->getId()
+                                'id' => $payment_method->getId(),
+                                'enabled' => $payment_method->isEnabled()
                             ]);
                         }
                         $list[$payment_method->getType()] = $payment_method->getId();
