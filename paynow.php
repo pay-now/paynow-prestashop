c38890f44b15393126e234f404993e09576fd56f
emilleszczak2 <emil.leszczak@intramedium.pl>
2021-12-07 14:35:39 +0100

    Release 1.5.2

--- paynow.php
+++ paynow.php
@@ -40,7 +40,7 @@ class Paynow extends PaymentModule
     {
         $this->name = 'paynow';
         $this->tab = 'payments_gateways';
-        $this->version = '1.5.1';
+        $this->version = '1.5.2';
         $this->ps_versions_compliancy = ['min' => '1.6.0', 'max' => _PS_VERSION_];
         $this->author = 'mElements S.A.';
         $this->is_eu_compatible = 1;
