9468e2909c71689d3f4305e013ea27c3a0170b5f
emilleszczak2 <emil.leszczak@intramedium.pl>
2021-12-29 14:26:09 +0100

    Fix admin js

--- views/js/admin.js
+++ views/js/admin.js
@@ -10,19 +10,18 @@
  **/
 $(document).ready(function () {
     let paymentValidityTimeSwitchOn = $('#PAYNOW_PAYMENT_VALIDITY_TIME_ENABLED_on');
-    let paymentValidityTimeSwitchOnLabel = $('label[for="PAYNOW_PAYMENT_VALIDITY_TIME_ENABLED_on"]');
-    let paymentValidityTimeSwitchOffLabel = $('label[for="PAYNOW_PAYMENT_VALIDITY_TIME_ENABLED_off"]');
+    let paymentValidityTimeSwitchOff = $('#PAYNOW_PAYMENT_VALIDITY_TIME_ENABLED_off');
     let paymentValidityTimeInput = $('input[name="PAYNOW_PAYMENT_VALIDITY_TIME"]');
 
     if (!paymentValidityTimeSwitchOn.is(':checked')) {
         paymentValidityTimeInput.prop('disabled', true);
     }
 
-    paymentValidityTimeSwitchOnLabel.on("click", function () {
+    paymentValidityTimeSwitchOn.on("change", function () {
         paymentValidityTimeInput.prop('disabled', false);
     });
 
-    paymentValidityTimeSwitchOffLabel.on("click", function () {
+    paymentValidityTimeSwitchOff.on("click", function () {
         paymentValidityTimeInput.prop('disabled', true);
     })
 });
