2569b7bb854014e2964e69b4d4b844794a18f693
emilleszczak2 <emil.leszczak@intramedium.pl>
2022-01-10 21:22:49 +0100

    Add missing column on init table

---
+++ upgrade/upgrade-1.6.3.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License (MIT)
+ * that is bundled with this package in the file LICENSE.md.
+ *
+ * @author mElements S.A.
+ * @copyright mElements S.A.
+ * @license   MIT License
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_1_6_3($module)
+{
+    try {
+        Db::getInstance()->execute(
+            "ALTER TABLE `" . _DB_PREFIX_ . "paynow_payments` 
+               ADD `total` DECIMAL(20,6) NOT NULL DEFAULT '0.000000' 
+               AFTER `status`"
+        );
+    } catch (PrestaShopDatabaseException $exception) {
+
+    }
+
+    return true;
+}
