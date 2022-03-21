3665487ac0db9424ea4d8fb1191d8a39d305dd53
Emil Leszczak <emil.leszczak@intramedium.pl>
2022-03-21 13:26:36 +0100

    Release 1.6.13 (#130)
    Remove github-api dependency

---
+++ classes/PaynowGithubClient.php
@@ -0,0 +1,53 @@
+<?php
+
+use Http\Discovery\Exception\NotFoundException;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Http\Discovery\Psr18ClientDiscovery;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+class PaynowGithubClient
+{
+    /** @var ClientInterface */
+    private $client;
+
+    /** @var RequestFactoryInterface */
+    protected $messageFactory;
+
+    /** @var StreamFactoryInterface */
+    protected $streamFactory;
+
+    /** @var \Psr\Http\Message\UriInterface */
+    private $url;
+
+    public function __construct()
+    {
+        try {
+            $this->client = Psr18ClientDiscovery::find();
+        } catch (NotFoundException $exception) {
+            $this->client = HttpClientDiscovery::find();
+        }
+        $this->messageFactory = Psr17FactoryDiscovery::findRequestFactory();
+        $this->streamFactory = Psr17FactoryDiscovery::findStreamFactory();
+        $this->url = Psr17FactoryDiscovery::findUrlFactory()->createUri('https://api.github.com');
+    }
+
+    public function latest($username, $repository)
+    {
+        $request = $this->messageFactory->createRequest(
+            'GET',
+            $this->url->withPath('/repos/'.rawurlencode($username).'/'.rawurlencode($repository).'/releases/latest')
+        );
+
+        try {
+            return json_decode($this->client->sendRequest($request)->getBody());
+        } catch (ClientExceptionInterface $exception) {
+            PaynowLogger::error("Error occurred during retrieving github latest release information {message={}}", $exception->getMessage());
+        }
+
+        return null;
+    }
+}
