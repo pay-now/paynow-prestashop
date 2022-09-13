d322a41b383c0bb6c1b6cac349d50c42a6f3652b
Krzysztof Ustowski <krzysztof.ustowski@movecloser.pl>
2022-09-14 00:52:03 +0200

    version bump

--- paynow.php
+++ paynow.php
@@ -43,7 +43,7 @@ class Paynow extends PaymentModule
     {
         $this->name = 'paynow';
         $this->tab = 'payments_gateways';
-        $this->version = '1.6.18';
+        $this->version = '1.6.20';
         $this->ps_versions_compliancy = ['min' => '1.6.0', 'max' => _PS_VERSION_];
         $this->author = 'mElements S.A.';
         $this->is_eu_compatible = 1;
