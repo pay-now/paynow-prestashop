7867820b3a0d0792cf7bf94529b28aaaa5885da8
Mateusz Twardowski <mateusz.twardowski@movecloser.pl>
2024-08-12 10:06:22 +0200

    up versions

--- paynow.php
+++ paynow.php
@@ -49,7 +49,7 @@ class Paynow extends PaymentModule
     {
         $this->name = 'paynow';
         $this->tab = 'payments_gateways';
-        $this->version = '1.7.7';
+        $this->version = '1.7.8';
         $this->ps_versions_compliancy = ['min' => '1.6.0', 'max' => _PS_VERSION_];
         $this->author = 'mElements S.A.';
         $this->is_eu_compatible = 1;
